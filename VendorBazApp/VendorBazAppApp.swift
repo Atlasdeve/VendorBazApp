4b0a5e21727fb4b8ff7fe3bd0b390c5e19a74f8e
Talhamahmood23 <Atlasdevelopers1@gmail.com>
2024-09-06 19:28:44 +0500

    Initial Commit

---
+++ VendorBazApp/VendorBazAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorBazAppApp.swift
+//  VendorBazApp
+//
+//  Created by Atlas on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct VendorBazAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
